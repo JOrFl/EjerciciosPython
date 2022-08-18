1e6d149bc09425c3d35439ba128fc65114cbf828
Juan Ortega <juanortegaf1997@gmail.com>
2022-08-17 19:27:44 -0500

    Actualizacion de curso

--- OperacionFunc.py
+++ OperacionFunc.py
@@ -1,3 +1,4 @@
+
 def suma(arg1,arg2):
     R = int(arg1) + int(arg2)
     return (R)
@@ -21,29 +22,35 @@ def division(arg1,arg2):
     return (R)
 
 
-
+H = []
 Bool = "Si"
 while(Bool == "Si"):
 
-    a = input("Introduzca primer sumando: ")
-    b = input("Introduzca segundo sumando: ")
-    Op = input("Que operacion desea realizar? ")
+    a = input("Introduzca primer factor: ")
+    b = input("Introduzca segundo factor: ")
+    Op = input("¿Que operacion desea realizar? ")
 
     if(Op == "Suma"):
         print("El resultado es {}".format(suma(a,b)))
+        H.append(suma(a,b))
 
     elif(Op == "Resta"):
         print("El resultado es {}".format(resta(a,b)))
+        H.append(resta(a,b))
 
     elif(Op == "Multiplicacion"):
         print("El resultado es {}".format(multiplicacion(a,b)))
+        H.append(multiplicacion(a,b))
 
     elif(Op == "Division"):
         print("El resultado es {}".format(division(a,b)))
-
-    else:
-        print("Debe introducir una operacion valida")
-
-    Bool = input("¿Desea realizar otra operacion? ")
+        H.append(division(a,b))
 
 
+    Hist = input("¿Desea desplegar el historial de resultados? ")
+
+    if(Hist == "Si"):
+        print(H)
+        Bool = input("¿Desea realizar otra operacion? ")
+    elif(Hist == "No"):
+        Bool = input("¿Desea realizar otra operacion? ")
