ccc0a95cea70a15490b714f0983a128b1bd4b50c
Juan Ortega <juanortegaf1997@gmail.com>
2022-08-17 19:37:55 -0500

    Actualizacion de curso

---
+++ clases.py
@@ -0,0 +1,12 @@
+class Michi():
+    def __init__(self, nombre, edad, color):
+        self.nombre = nombre
+        self.edad = edad
+        self.color = color
+    
+    def maullar(self):
+        print("Rauuuuuul")
+
+Michigordo = Michi("Michigordo", 5, "negro")
+
+Michigordo.maullar()
